9d98c467bdcc24c8810376eb01385343414a6bc2
ayogsynergy <sanchezg.eng@gmail.com>
2024-04-28 11:06:07 -0400

    fill in missing data for salary

--- data_preprocessing_template.R
+++ data_preprocessing_template.R
@@ -4,8 +4,15 @@ dataset = read.csv("Data.csv")
 # Clean up missing data
 # Check if value in Age column is missing
 dataset$Age = ifelse(
-                      is.na(dataset$Age),
-                      # calculuate avereage and ask to add missing values
-                      ave(dataset$Age, FUN = function(x) mean(x, na.rm = TRUE)),
-                      dataset$Age
+                  is.na(dataset$Age),
+                  # calculate average and ask to add missing values
+                  ave(dataset$Age, FUN = function(x) mean(x, na.rm = TRUE)),
+                  dataset$Age
+)
+
+dataset$Salary = ifelse(
+  is.na(dataset$Salary),
+  # calculate average and ask to add missing values
+  ave(dataset$Salary, FUN = function(x) mean(x, na.rm = TRUE)),
+  dataset$Salary
 )
