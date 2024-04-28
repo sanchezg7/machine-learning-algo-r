cf1b35fc9a0adfbdcf1b50e3df1ef93de303180a
ayogsynergy <sanchezg.eng@gmail.com>
2024-04-28 16:14:05 -0400

    finalizing linear regression section

--- Part 20 - Regression/Section 4 - Simple Linear Regression/data_preprocessing_template.R
+++ Part 20 - Regression/Section 4 - Simple Linear Regression/data_preprocessing_template.R
@@ -24,9 +24,22 @@ y_pred_test_set = predict(regressor, newdata = test_set)
 y_pred_training_set = predict(regressor, newdata = training_set)
 
 library(ggplot2)
+# Training set
 ggplot() + 
   geom_point(aes(x = training_set$YearsExperience, y = training_set$Salary), 
              colour = 'red') +
   geom_line(aes(x = training_set$YearsExperience, y = y_pred_training_set),
             colour = 'blue') + 
-  ggtitle("Salary vs Experience(Training set)")
+  ggtitle("Salary vs Experience(Training set)") +
+  xlab("Years of exeperince") +
+  ylab("Salary")
+
+# Test set - this reveals that there is statistical significance. We discover it's not 100% correlated, tho. Linearly correlated.
+ggplot() + 
+  geom_point(aes(x = test_set$YearsExperience, y = test_set$Salary), 
+             colour = 'red') +
+  geom_line(aes(x = training_set$YearsExperience, y = y_pred_training_set),
+            colour = 'blue') + 
+  ggtitle("Salary vs Experience(Test set)") +
+  xlab("Years of exeperince") +
+  ylab("Salary")
