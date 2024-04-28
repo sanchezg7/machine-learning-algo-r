e3865259344795c7804a4652f710906440cafc69
ayogsynergy <sanchezg.eng@gmail.com>
2024-04-28 11:04:26 -0400

    fill in missing age by calculating the mean

--- data_preprocessing_template.R
+++ data_preprocessing_template.R
@@ -1,1 +1,11 @@
+# Imports the dataset. You can view it on the right hand side of RStudio
 dataset = read.csv("Data.csv")
+
+# Clean up missing data
+# Check if value in Age column is missing
+dataset$Age = ifelse(
+                      is.na(dataset$Age),
+                      # calculuate avereage and ask to add missing values
+                      ave(dataset$Age, FUN = function(x) mean(x, na.rm = TRUE)),
+                      dataset$Age
+)
