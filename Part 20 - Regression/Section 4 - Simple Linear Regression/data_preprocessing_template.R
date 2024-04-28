d150502bdf205680896f29796cf1fab9c0b6f29c
ayogsynergy <sanchezg.eng@gmail.com>
2024-04-28 15:51:38 -0400

    test set

--- Part 20 - Regression/Section 4 - Simple Linear Regression/data_preprocessing_template.R
+++ Part 20 - Regression/Section 4 - Simple Linear Regression/data_preprocessing_template.R
@@ -4,16 +4,15 @@
 # Independent variable is the years experience. Dependent is salary
 dataset = read.csv('Salary_Data.csv')
 
-
-
 # Splitting the dataset into the Training set and Test set
 # install.packages('caTools')
 library(caTools)
 set.seed(123)
-split = sample.split(dataset$DependentVariable, SplitRatio = 0.8)
+split = sample.split(dataset$Salary, SplitRatio = 2/3)
 training_set = subset(dataset, split == TRUE)
 test_set = subset(dataset, split == FALSE)
 
 # Feature Scaling
 # training_set = scale(training_set)
 # test_set = scale(test_set)
+
