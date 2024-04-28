e9fff6242ff5c4902cfa04a2b9a6a795dcfd908f
ayogsynergy <sanchezg.eng@gmail.com>
2024-04-28 15:23:36 -0400

    updating script to be deterministic

--- data_preprocessing_template.R
+++ data_preprocessing_template.R
@@ -35,3 +35,7 @@ split = sample.split(dataset$Purchased, SplitRatio = 0.8)
 training_set = subset(dataset, split == TRUE)
 test_set = subset(dataset, split == FALSE)
 
+# Feature Scaling
+training_set = scale(training_set)
+test_set = scale(test_set)
+
